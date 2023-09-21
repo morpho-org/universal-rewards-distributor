f38d03bc4b873e8b6dc7e5de475cb06da11f96d3
julien <julien@morpho.xyz>
2023-09-21 09:03:21 +0200

    refactor(urd): make error messages shorter

--- src/libraries/ErrorsLib.sol
+++ src/libraries/ErrorsLib.sol
@@ -7,17 +7,17 @@ pragma solidity ^0.8.0;
 /// @notice Library exposing error messages.
 library ErrorsLib {
     string internal constant CALLER_NOT_OWNER_OR_UPDATER =
-        "UniversalRewardsDistributor: caller is not the owner or updater";
+        "Caller is not the owner or updater";
 
-    string internal constant CALLER_NOT_OWNER = "UniversalRewardsDistributor: caller is not the owner";
+    string internal constant CALLER_NOT_OWNER = "Caller is not the owner";
 
-    string internal constant NO_PENDING_ROOT = "UniversalRewardsDistributor: no pending root";
+    string internal constant NO_PENDING_ROOT = "No pending root";
 
-    string internal constant TIMELOCK_NOT_EXPIRED = "UniversalRewardsDistributor: timelock is not expired";
+    string internal constant TIMELOCK_NOT_EXPIRED = "Timelock is not expired";
 
-    string internal constant ROOT_NOT_SET = "UniversalRewardsDistributor: root is not set";
+    string internal constant ROOT_NOT_SET = "Root is not set";
 
-    string internal constant INVALID_PROOF_OR_EXPIRED = "UniversalRewardsDistributor: invalid proof or expired";
+    string internal constant INVALID_PROOF_OR_EXPIRED = "Invalid proof or expired";
 
-    string internal constant ALREADY_CLAIMED = "UniversalRewardsDistributor: already claimed";
+    string internal constant ALREADY_CLAIMED = "Already claimed";
 }
